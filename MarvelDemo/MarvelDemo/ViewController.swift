eff9050fa4970bbb318943a1da87885bbfe007b6
Alessandro Martin <martin.acg@gmail.com>
2020-04-21 13:34:35 +0100

    Xcode Project Creation

---
+++ MarvelDemo/MarvelDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarvelDemo
+//
+//  Created by Alessandro Martin on 21/04/2020.
+//  Copyright © 2020 Alessandro Martin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
